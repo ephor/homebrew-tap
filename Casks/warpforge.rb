f1ffb814b54cfe0c8108b7e85eb44d27e69704b1
warpforge-ci <ci@warpforge.local>
2026-08-21 14:07:31 +0000

    warpforge 0.7.0

--- Casks/warpforge.rb
+++ Casks/warpforge.rb
@@ -1,6 +1,6 @@
 cask "warpforge" do
-  version "0.6.8"
-  sha256 "ac14adefe9b30dfabd0a451d319dbf3d36afb8459441e6610412a7e17aac36d6"
+  version "0.7.0"
+  sha256 "e509c4a55d0720dd48a2ce3e9316613adf3c05b3686d789066f5b7e6b945659f"
 
   url "https://github.com/ephor/warpforge/releases/download/v#{version}/Warpforge_#{version}_aarch64.dmg"
   name "Warpforge"
